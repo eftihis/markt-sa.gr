62c19ad1362bb9fa4f141a346a5c9cf72e822cc3
Eftihis <eftihisluke@gmail.com>
2025-01-26 16:27:05 +0200

    Add small animation

--- Animations/GSAP/For-sale.js
+++ Animations/GSAP/For-sale.js
@@ -17,6 +17,12 @@ document.addEventListener('DOMContentLoaded', () => {
             duration: 0.6,
             ease: "power2.out"
         }, "<0.3")
+        .from("#projects-subtitle", {
+            opacity: 0,
+            y: 20,
+            duration: 0.6,
+            ease: "power2.out"
+          }, "<0.3")
         .set('[gsap-animate-secondary]', {
             autoAlpha: 1,
         }, "<")
